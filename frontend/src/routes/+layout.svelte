9db4f172642d34447521f3280411d3943d810b5f
Tali Auster <me@tali.network>
2025-05-05 21:24:48 -0700

    feat: init new UI
    Just throwing stuff at a wall.

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		max-width: 8.5in;
+		margin: auto;
+		font-family: 'Sans Serif';
+	}
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+	import Dropzone from 'svelte-file-dropzone';
+
+	interface Files {
+		accepted: any[];
+		rejected: any[];
+	}
+	let files: Files = $state({
+		accepted: [],
+		rejected: []
+	});
+
+	function handleFilesSelect(e) {
+		const { acceptedFiles, fileRejections } = e.detail;
+		files.accepted = [...files.accepted, ...acceptedFiles];
+		files.rejected = [...files.rejected, ...fileRejections];
+	}
+</script>
+
+<form action="/files/upload" method="post" enctype="multipart/form-data">
+	<div class="file-menu">
+		<Dropzone containerClasses="file-input" on:drop={handleFilesSelect}>
+			<span class="form-copy">Select files.</span>
+		</Dropzone>
+
+		<input type="submit" class="submit" value="Upload files." />
+	</div>
+
+	<ul class="files">
+		{#each files.accepted as file}
+			<li>{file.path}</li>
+		{/each}
+	</ul>
+	<ul class="files">
+		{#each files.rejected as file}
+			<li>{file.path}</li>
+		{/each}
+	</ul>
+</form>
+
+<style>
+	.file-menu {
+		display: flex;
+		flex-direction: row;
+		column-gap: 0.35rem;
+	}
+
+	.submit {
+		border: 2px solid #ccc;
+		border-radius: 0.25rem;
+		background-color: #eaffe4;
+		padding-left: 1rem;
+		padding-right: 1rem;
+
+		font-size: inherit;
+	}
+
+	.submit:hover {
+		cursor: pointer;
+	}
+
+	:global {
+		.file-input {
+			border: 2px solid #ccc !important;
+			border-radius: 0.25rem !important;
+			background-color: #f2f2f2 !important;
+			align-items: start !important;
+			padding: 1rem !important;
+		}
+
+		.file-input:hover {
+			cursor: pointer;
+		}
+
+		.files:has(li) {
+			max-width: 100%;
+			padding: 1rem;
+			margin-top: 1rem;
+			background-color: #f9f9f9;
+			border: 2px solid #ddd;
+			border-radius: 0.25rem;
+
+			display: flex;
+			row-gap: 0.35rem;
+			flex-direction: column;
+		}
+
+		.files > li {
+			list-style: none;
+		}
+
+		.files > li:hover {
+			color: #22391b;
+		}
+	}
+
+	.form-copy {
+		color: black;
+	}
+</style>
