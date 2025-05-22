7701276ee1f6253696647ff90350316c0eb423a9
Tali Auster <me@tali.network>
2025-05-22 14:50:24 -0600

    chore: fmt flake.nix

--- flake.nix
+++ flake.nix
@@ -1,44 +1,56 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.backend =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in import ./backend { inherit pkgs; };
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages.x86_64-linux.backend =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        import ./backend { inherit pkgs; };
 
-    packages.x86_64-linux.frontend =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in import ./frontend { inherit pkgs; };
+      packages.x86_64-linux.frontend =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        import ./frontend { inherit pkgs; };
 
-    devShells.x86_64-linux.default =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in import ./shell.nix { inherit pkgs; };
+      devShells.x86_64-linux.default =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        import ./shell.nix { inherit pkgs; };
 
-    nixosModules.cabinet =
-      { config, pkgs, ... }: {
-        networking.firewall.allowedTCPPorts = [ 80 ];
+      nixosModules.cabinet =
+        { config, pkgs, ... }:
+        {
+          networking.firewall.allowedTCPPorts = [ 80 ];
 
-        systemd.services.cabinet-frontend = {
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network.target" "cabinet-backend.target" ];
-          description = "Cabinet's frontend component.";
-          serviceConfig = {
-            ExecStart = "${pkgs.nodejs}/bin/node ${self.packages.x86_64-linux.frontend}/lib/cabinet-frontend";
+          systemd.services.cabinet-frontend = {
+            wantedBy = [ "multi-user.target" ];
+            after = [
+              "network.target"
+              "cabinet-backend.target"
+            ];
+            description = "Cabinet's frontend component.";
+            serviceConfig = {
+              ExecStart = "${pkgs.nodejs}/bin/node ${self.packages.x86_64-linux.frontend}/lib/cabinet-frontend";
+            };
+
+            environment.PORT = "6445";
           };
 
-	  environment.PORT = "6445";
-        };
+          systemd.services.cabinet-backend = {
+            wantedBy = [ "multi-user.target" ];
+            after = [ "network.target" ];
+            description = "file storage web server";
+            serviceConfig = {
+              ExecStart = "${self.packages.x86_64-linux.backend}/bin/cabinet-srv";
+            };
 
-        systemd.services.cabinet-backend = {
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network.target" ];
-          description = "file storage web server";
-          serviceConfig = {
-            ExecStart = "${self.packages.x86_64-linux.backend}/bin/cabinet-srv";
+            environment.CABINET_PORT = "6446";
           };
-
-	  environment.CABINET_PORT = "6446";
         };
-      };
-  };
+    };
 }
